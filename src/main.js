b13e90046c5c8740c780c8483922c3b63ccf7c19
bwyfed <36172659@qq.com>
2017-08-25 20:12:45 +0800

    加入项目所需的静态资源。

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {sum,minus} from './util';
-
-console.log(`sum:${sum(1,6)}`);
-console.log(`minus:${minus(10,2)}`);
 
 Vue.config.productionTip = false
 
