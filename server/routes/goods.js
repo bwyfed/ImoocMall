9bfbe38195e8eba84ba1101cb03676edbc4c5054
bwyfed <36172659@qq.com>
2017-11-26 16:25:15 +0800

    对商品列表实现分页和排序功能。

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -22,8 +22,35 @@ mongoose.connection.on("disconnected", function(){
 });
 //查询商品列表信息
 router.get("/",function(req,res,next){
+  let page = parseInt(req.param("page"));//获取当前页面号码
+  let pageSize = parseInt(req.param("pageSize")); //每页的条目数
+  let sort = req.param("sort"); //排序是升序还是降序
+  let skip = (page-1)*pageSize; //跳过的条数
+  let params = {};
+  //分页功能:查找所有的数据，跳过skip条数据，并限制每页是pageSize条数据
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+  //排序功能
+  goodsModel.sort({'salePrice':sort});
+  goodsModel.exec(function(err,doc){
+    if(err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    } else {
+      res.json({
+        status: 0,
+        msg: '',
+        result: {
+          count: doc.length,
+          list: doc
+        }
+      })
+    }
+  });
   // res.send("hello, goods list.");
   //查询数据库
+  /*
   Goods.find({},function(err,doc) {
     if(err) {
       res.json({
@@ -41,6 +68,7 @@ router.get("/",function(req,res,next){
       })
     }
   });
+  */
 });
 
 module.exports = router;
