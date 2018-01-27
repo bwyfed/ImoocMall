b867d3c1a082aab78e38cd9042b77957200273f5
bwyfed <36172659@qq.com>
2018-01-27 21:16:36 +0800

    在商品列表页面中实现排序箭头的升序和降序过渡效果，实现“加入购物车”按钮的悬浮样式。

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -4,72 +4,64 @@
     <nav-bread>
       <span>Goods</span>
     </nav-bread>
-    <div class="accessory-result-page accessory-page">
-      <div class="container">
-        <div class="filter-nav">
-          <span class="sortby">Sort by:</span>
-          <span class="def">Default</span>
-          <a href="javascript:void(0)" class="price" @click="sortGoods">
-            Price
-            <svg class="icon-arrow-short sort-up">
-              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-arrow-short"></use>
-            </svg>
-          </a>
-          <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
+    <div class="container">
+      <div class="filter-nav">
+        <span class="sortby">Sort by:</span>
+        <span class="def">Default</span>
+        <a href="javascript:void(0)" class="price" @click="sortGoods">
+          Price
+          <svg class="icon-arrow-short" v-bind:class="{'sort-up':sortFlag}">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-arrow-short"></use>
+          </svg>
+        </a>
+        <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
+      </div>
+      <div class="accessory-result">
+        <!-- filter 当视口小于一定值时，此区域会被隐藏掉 -->
+        <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
+          <dl class="filter-price">
+            <dt>Price:</dt>
+            <dd><a href="javascript:void(0)" @click="setPriceFilter('all')" v-bind:class="{cur: priceChecked=='all'}">All</a></dd>
+            <dd v-for="(price,index) in priceFilter">
+              <a href="javascript:void(0)" @click="setPriceFilter(index)" v-bind:class="{cur:priceChecked==index}">{{price.startPrice}} - {{price.endPrice}}</a>
+            </dd>
+          </dl>
         </div>
-        <div class="accessory-result">
-          <!-- filter 当视口小于一定值时，此区域会被隐藏掉 -->
-          <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
-            <dl class="filter-price">
-              <dt>Price:</dt>
-              <dd><a href="javascript:void(0)" @click="setPriceFilter('all')" v-bind:class="{cur: priceChecked=='all'}">All</a></dd>
-              <dd v-for="(price,index) in priceFilter">
-                <a href="javascript:void(0)" @click="setPriceFilter(index)" v-bind:class="{cur:priceChecked==index}">{{price.startPrice}} - {{price.endPrice}}</a>
-              </dd>
-            </dl>
-          </div>
 
-          <!-- search result accessories list -->
-          <div class="accessory-list-wrap">
-            <div class="accessory-list col-4">
-              <ul>
-                <li v-for="item in goodsList">
-                  <div class="pic">
-                    <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
+        <!-- search result accessories list -->
+        <div class="accessory-list-wrap">
+          <div class="accessory-list col-4">
+            <ul>
+              <li v-for="item in goodsList">
+                <div class="pic">
+                  <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
+                </div>
+                <div class="main">
+                  <div class="name">{{item.productName}}</div>
+                  <!--<div class="price">{{item.productPrice}}</div>-->
+                  <div class="price">{{item.salePrice}}</div>
+                  <div class="btn-area">
+                    <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                   </div>
-                  <div class="main">
-                    <div class="name">{{item.productName}}</div>
-                    <!--<div class="price">{{item.productPrice}}</div>-->
-                    <div class="price">{{item.salePrice}}</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <!--
-                <li v-for="item in goodsList">
-                  <div class="pic">
-                    <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">{{item.productName}}</div>
-                    <div class="price">{{item.salePrice}}</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                -->
-              </ul>
-              <div class="load-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-                <img v-if="loading" src="../assets/loading-spinning-bubbles.svg"/>
-              </div>
+                </div>
+              </li>
+            </ul>
+            <div class="load-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+              <img v-if="loading" src="../assets/loading-spinning-bubbles.svg"/>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+
+    <modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+      <p slot="message">
+        请先登录，否则无法加入到购物车中！
+      </p>
+      <div slot="btnGroup">
+        <a class="btn btn--m" href="javascript:;" @click="mdShow=false;">关闭</a>
+      </div>
+    </modal>
     <nav-footer></nav-footer>
   </div>
 </template>
@@ -208,6 +200,7 @@
   import NavHeader from '@/components/NavHeader.vue'
   import NavFooter from '@/components/NavFooter.vue'
   import NavBread from '@/components/NavBread.vue'
+  import Modal from '@/components/Modal.vue'
   import axios from 'axios'
   export default {
       data() {
@@ -218,6 +211,7 @@
             pageSize: 8,  //每页显示的条目数
             busy: true,
             loading: false, //是否显示加载效果
+            mdShow: false,  //是否显示模态对话框
             priceFilter: [
               {
                   startPrice: '0.00',
@@ -244,7 +238,8 @@
       components: {
         NavHeader,
         NavFooter,
-        NavBread
+        NavBread,
+        Modal
       },
       mounted: function() {
           this.getGoodsList();
@@ -320,7 +315,7 @@
                 if(res.status===0) {
                     alert("加入成功");
                 } else {
-                    alert("msg:"+res.msg);
+                    this.mdShow = true;
                 }
             })
         },
@@ -331,6 +326,9 @@
         closePop() {
           this.filterBy = false;
           this.overLayFlag = false;
+        },
+        closeModal() {
+            this.mdShow = false;
         }
       }
   }
